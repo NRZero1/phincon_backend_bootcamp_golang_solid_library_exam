bb59d7be82c2de378e6aa69d2fdd529a2748d5cd
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-21 00:13:43 +0700

    Add SOLID docs and resolve merge conflict (#10)
    * chore: adding explaination about SOLID

--- internal/handler/book_handler_impl.go
+++ internal/handler/book_handler_impl.go
@@ -9,12 +9,17 @@ type BookHandler struct {
 	bookUseCase usecase.BookUseCaseInterface
 }
 
+
+// Implement D in Solid, using high level abstraction so it will not depend on specific Book Use Case
+// All of the function below doesn't need to change anymore but open if book handler want to extend more function (implementing O)
+// by using interface as a type it will also implementing L in solid because interface is like its parent class, so replacing book handler interface with its child book handler won't break the app
 func NewBookHandler(bookUseCase usecase.BookUseCaseInterface) BookHandlerInterface {
 	return BookHandler{
 		bookUseCase: bookUseCase,
 	}
 }
 
+// each of this function have single responsibility implementing S
 func (h BookHandler) Save(book domain.Book) (domain.Book, error) {
 	savedBook, err := h.bookUseCase.Save(book)
 
