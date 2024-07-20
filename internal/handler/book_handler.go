54fedfe3f16085aa0d243ae2023c6c242c296813
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-20 18:29:49 +0700

    Feature find by id and find by title (#6)
    * feat: book find by id and find by title
    
    * fix: fix find by title only receive one book even if there are two or more book with the same title
    
    * fix: fix return value find by title

--- internal/handler/book_handler.go
+++ internal/handler/book_handler.go
@@ -6,8 +6,18 @@ import (
 
 type BookHandlerInterface interface {
 	BookSave
+	BookFindById
+	BookFindByTitle
 }
 
 type BookSave interface {
 	Save(domain.Book) (domain.Book, error)
+}
+
+type BookFindById interface {
+	FindById(int) (domain.Book, error)
+}
+
+type BookFindByTitle interface {
+	FindByTitle(string) ([]domain.Book, error)
 }
--- internal/repository/book_repository.go
+++ internal/repository/book_repository.go
@@ -5,10 +5,20 @@ import "solid_library_exam/internal/domain"
 type BookRepositoryInterface interface {
 	BookSave
 	BookUpdateStatus
+	BookFindById
+	BookFindByTitle
 }
 
 type BookSave interface {
 	Save(*domain.Book) (domain.Book, error)
+}
+
+type BookFindById interface {
+	FindById(int) (domain.Book, error)
+}
+
+type BookFindByTitle interface {
+	FindByTitle(string) ([]domain.Book, error)
 }
 
 type BookUpdateStatus interface {
